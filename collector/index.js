a286c766336e1a155c5e1eaa9d3358e612fa1964
tinbao <tinbao27@hotmail.com>
2023-06-05 23:04:15 +1000

    feat: working checkmark overlay on top of image using css

--- collector/index.js
+++ collector/index.js
@@ -1,51 +1,46 @@
 // Constant with Pokemon pack images
-var pokemonTCGBoosterPacks = 
+const pokemonTCGBoosterPacks = 
 [
-    ["Pack Name", "1", "2", "3", "4"]
-    ["LOST ORIGIN", "lost_origin_1", "lost_origin_2", "lost_origin_3", "lost_origin_4"]
+    ["swsh11_logo", "lost_origin_1", "lost_origin_2", "lost_origin_3", "lost_origin_4"],
+    ["swsh12_logo", "silver_tempest_1", "silver_tempest_2", "silver_tempest_3", "silver_tempest_4"],
+    ["swsh12pt5_logo", "crown_zenith_1"]
 ]
 
-// Create a new table to store the Pokemon TCG booster pack images.
+// Create a new table to store the booster pack images.
 var table = document.createElement("table");
 
-// Add a header row to the table
-const headerRow = document.createElement("tr");
-for (var i = 0; i < pokemonTCGBoosterPacks[0].length; i++) {
-  const headerCell = document.createElement("td");
-  headerCell.textContent = key;
-  headerRow.appendChild(headerCell);
-}
-table.appendChild(headerRow);
-
-// Loop through the Pokemon TCG booster pack images and add them to the table.
-for (var i = 1; i < pokemonTCGBoosterPacks.length - 1; i++) {
+// Loop through the booster pack images and add them to the table.
+for (var i = 0; i < pokemonTCGBoosterPacks.length; i++) {
   var row = document.createElement("tr");
 
   // Add the image to the row.
-  for (var j = 0; j < pokemonTCGBoosterPacks[i].length - 1; j++) {
-    var image = document.createElement("td");
-    // Set a uniform size
-    image.style.maxWidth = '100px';
-    image.style.maxHeight = '100px';
-    image.innerHTML = "<img src=resources/" + pokemonTCGBoosterPacks[i][j] + ".jpg>";
-    row.appendChild(image);
+  for (var j = 0; j < pokemonTCGBoosterPacks[i].length; j++) {
+    if (j == 0) {
+        var image = document.createElement("td");
+        image.innerHTML = "<img src=resources/" + pokemonTCGBoosterPacks[i][j] + ".jpg class=logo>";
+        image.id = pokemonTCGBoosterPacks[i][j];
+        row.appendChild(image);
+        continue;
+    }
+
+    var cell = document.createElement("td");
+
+    var input = document.createElement("input");
+    input.type = "checkbox";
+    input.id = pokemonTCGBoosterPacks[i][j];
+    cell.appendChild(input);
+
+    var label = document.createElement("label");
+    label.className = "custom-checkbox";
+    label.htmlFor = pokemonTCGBoosterPacks[i][j];
+    label.innerHTML = "<img src=resources/" + pokemonTCGBoosterPacks[i][j] + ".jpg class=booster>";
+
+    cell.appendChild(label);
+    row.appendChild(cell);
   }
 
   // Add the row to the table.
   table.appendChild(row);
-}
-
-// Scale the images to 100px
-for (const image of table.querySelectorAll("img")) {
-    image.style.width = "100px";
-}
-
-// Set the onclick event for each row to toggle the color of the image
-for (const row of table.querySelectorAll("tr")) {
-  row.addEventListener("click", () => {
-    const image = row.querySelector("img");
-    image.style.color = image.style.color === "black" ? "white" : "black";
-  });
 }
 
 // Add the table to the document.
